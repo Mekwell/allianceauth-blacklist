3343cbc080a4e4224f43b348f7ffff52b5af66f6
Mekwell <mekwell@hotmail.com>
2025-11-13 14:44:11 +1100

    Changed to use X-API

--- blacklist/apiviews.py
+++ blacklist/apiviews.py
@@ -1,17 +1,17 @@
 from rest_framework import generics
-from rest_framework.authentication import TokenAuthentication
+from .authentication import CustomTokenAuthentication
 from .models import EveNote
 from .serializers import EveNoteSerializer
 
 class EveNoteList(generics.ListAPIView):
     queryset = EveNote.objects.filter(blacklisted=True)
     serializer_class = EveNoteSerializer
-    authentication_classes = [TokenAuthentication]
+    authentication_classes = [CustomTokenAuthentication]
     permission_classes = []  # No permissions required, only a valid token
 
 class EveNoteRetrieve(generics.RetrieveAPIView):
     queryset = EveNote.objects.filter(blacklisted=True)
     serializer_class = EveNoteSerializer
-    authentication_classes = [TokenAuthentication]
+    authentication_classes = [CustomTokenAuthentication]
     permission_classes = []  # No permissions required, only a valid token
     lookup_field = 'eve_id'
