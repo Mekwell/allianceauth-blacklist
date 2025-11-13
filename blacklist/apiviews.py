3daffc3f7e0fbb87e79ed8b01641309a2b21877a
Mekwell <mekwell@hotmail.com>
2025-11-14 00:01:49 +1100

    Update apiviews.py

--- blacklist/apiviews.py
+++ blacklist/apiviews.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
 from .models import EveNote
 from .serializers import EveNoteSerializer
 
@@ -8,11 +8,11 @@ class EveNoteList(generics.ListAPIView):
     queryset = EveNote.objects.filter(blacklisted=True)
     serializer_class = EveNoteSerializer
     authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
 
 class EveNoteRetrieve(generics.RetrieveAPIView):
     queryset = EveNote.objects.filter(blacklisted=True)
     serializer_class = EveNoteSerializer
     authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
     lookup_field = 'eve_id'
